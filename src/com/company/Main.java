cea88a5bd9d8264ec482914b4dea61c8ce291c1c
Fidan Ahmadova <fidan.ahmadova@Expressbank.az>
2023-04-13 14:46:15 +0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Main {
+
+    private static int a;
+    private static double b;
+    private static float c;
+    private static char d;
+    private static boolean e;
+    private static short f;
+    private static byte g;
+    private static long h = 678L;
+
+    public static void main(String[] der) {
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+    }
+}
